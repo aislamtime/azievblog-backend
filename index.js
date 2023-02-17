402583c2acf88f9b66f01ee237e900d94c3e5958
aislamtime <116115530+aislamtime@users.noreply.github.com>
2023-02-17 13:41:53 +0300

    Server run

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.listen(4444, (err) => {
+  if (err) console.log(err)
+  console.log('Server is run!')
+})
