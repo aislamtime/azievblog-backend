92722ee470734787bf30eff798399aa007047cc9
aislamtime <116115530+aislamtime@users.noreply.github.com>
2023-02-17 15:26:56 +0300

    Подключил базу данных и создал модель юзера

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+import { validationResult } from 'express-validator'
+import { registerValidation } from './validations/auth.js'
+
+mongoose
+  .connect(
+    'mongodb+srv://timeline:timeline@blog-mern.es1mwrz.mongodb.net/?retryWrites=true&w=majority',
+  )
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err))
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) return res.status(400).json(errors.array())
+
+  res.json({
+    success: true,
+  })
 })
 
 app.listen(4444, (err) => {
